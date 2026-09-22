e63c80e38485d3ed26807b5bf9793e7b0f2d4e7f
lubhanshu1 <lubhanshu17@gmail.com>
2026-09-22 15:13:03 +0530

    fix: load futuristic application stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Comparison from "./pages/Comparison"
+import "./App.css"
 
 export default function App() {
   return (
